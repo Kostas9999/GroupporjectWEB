43a105e6a7da0df39e77e86040e11419862ac631
Kostas9999 <b00148740@mytudublin.ie>
2022-12-17 19:15:18 +0000

    d

--- groupprojectweb/pages/api/session/session.js
+++
@@ -1,9 +0,0 @@
-import { withIronSessionApiRoute } from "iron-session/next";
-import { ironOptions } from "./session_Config";
-
-export default withIronSessionApiRoute(loginRoute, ironOptions);
-
-async function loginRoute(req, res) {
-  await res.status(200).json(req.session);
-  // await res.send(req.session);
-}
--- groupprojectweb/pages/api/session/session_Config.js
+++
@@ -1,8 +0,0 @@
-export const ironOptions = {
-  cookieName: "MoonTool_cookie",
-  password: process.env.SESSION_KEY,
-
-  cookieOptions: {
-    secure: process.env.NODE_ENV === "production",
-  },
-};
