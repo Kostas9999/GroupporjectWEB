9490099878655b405c0ff3ddf70ec43159bf86de
Kostas9999 <b00148740@mytudublin.ie>
2022-12-19 18:05:05 +0000

    Update myDevices.js

--- pages/api/myDevices.js
+++ pages/api/myDevices.js
@@ -1,5 +1,5 @@
 import { withIronSessionApiRoute } from "iron-session/next";
-import { ironOptions } from "./session_Config";
+import { ironOptions } from "./session/session_Config";
 
 export default withIronSessionApiRoute(function userRoute(req, res) {
   res.send({ user: req.session });
