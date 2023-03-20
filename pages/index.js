96ee4914b226b521d8eb6fe2984dbb73e99a5f04
KostuX <goldiskiker@gmail.com>
2023-03-20 22:42:22 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,6 @@ import {
 } from "@nextui-org/react";
 
 import React from "react";
-import { Hidden } from "@mui/material";
 
 export default function Home({ session }) {
   session = JSON.parse(session);
