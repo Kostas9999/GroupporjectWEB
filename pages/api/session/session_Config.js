30cf7a9101eb61bb2e4b048f3e1bdc7dc2426f13
KostuX <goldiskiker@gmail.com>
2023-03-26 15:15:27 +0100

    Update session_Config.js

--- pages/api/session/session_Config.js
+++ pages/api/session/session_Config.js
@@ -5,6 +5,6 @@ export const ironOptions = {
   cookieOptions: {
     secure: process.env.NODE_ENV === "production",
     // domain: "montool.vercel.app",
-    maxAge: (ttl === 0 ? 2147483647 : ttl) - 60,
+    maxAge: (ttl === 0 ? 3600 : ttl) - 60,
   },
 };
